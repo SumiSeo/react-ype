033a739ae7dedcfe4504c70abb20a19ca03443ed
Sumi <qkobr94@gmail.com>
2022-11-21 23:01:39 +0900

    add animation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,28 +2,35 @@ import "./Layout.scss";
 
 const Layout = (): JSX.Element => {
   return (
+    // <div className="layout">
+    //   <div className="layout__column">
+    //     <div className="layout__row">1</div>
+    //     <div className="layout__row">2</div>
+    //     <div className="layout__row">3</div>
+    //     <div className="layout__row">4</div>
+    //     <div className="layout__row">5</div>
+    //     <div className="layout__row">6</div>
+    //   </div>
+    //   <div className="layout__column">
+    //     <div className="layout__row">1</div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row">3</div>
+    //   </div>
+    //   <div className="layout__column">
+    //     <div className="layout__row">1</div>
+    //     <div className="layout__row">2</div>
+    //     <div className="layout__row">3</div>
+    //     <div className="layout__row">4</div>
+    //     <div className="layout__row">5</div>
+    //     <div className="layout__row">6</div>
+    //   </div>
+    // </div>
     <div className="layout">
-      <div className="layout__column">
-        <div className="layout__row">1</div>
-        <div className="layout__row">2</div>
-        <div className="layout__row">3</div>
-        <div className="layout__row">4</div>
-        <div className="layout__row">5</div>
-        <div className="layout__row">6</div>
-      </div>
-      <div className="layout__column">
-        <div className="layout__row">1</div>
-        <div className="layout__row"></div>
-        <div className="layout__row">3</div>
-      </div>
-      <div className="layout__column">
-        <div className="layout__row">1</div>
-        <div className="layout__row">2</div>
-        <div className="layout__row">3</div>
-        <div className="layout__row">4</div>
-        <div className="layout__row">5</div>
-        <div className="layout__row">6</div>
-      </div>
+      <div className="layout__column"></div>
+      <div className="layout__column"></div>
+      <div className="layout__column"></div>
+      <div className="layout__column"></div>
+      <div className="layout__column"></div>
     </div>
   );
 };
