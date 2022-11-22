0a2c90109a59729a5df1e8bd2f5f078e2203811d
Sumi <qkobr94@gmail.com>
2022-11-22 22:41:39 +0900

    try with grid and animation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,54 +2,55 @@ import "./Layout.scss";
 
 const Layout = (): JSX.Element => {
   return (
-    // <div className="layout">
-    //   <div className="layout__column">
-    //     <div className="layout__row">1</div>
-    //     <div className="layout__row">2</div>
-    //     <div className="layout__row">3</div>
-    //     <div className="layout__row">4</div>
-    //     <div className="layout__row">5</div>
-    //     <div className="layout__row">6</div>
-    //   </div>
-    //   <div className="layout__column">
-    //     <div className="layout__row">1</div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row">3</div>
-    //   </div>
-    //   <div className="layout__column">
-    //     <div className="layout__row">1</div>
-    //     <div className="layout__row">2</div>
-    //     <div className="layout__row">3</div>
-    //     <div className="layout__row">4</div>
-    //     <div className="layout__row">5</div>
-    //     <div className="layout__row">6</div>
-    //   </div>
-    // </div>
     <div className="layout">
       <div className="layout__column">
+        <div className="layout__row--absolute"></div>
         <div className="layout__row">SUMI SEO</div>
         <div className="layout__row"></div>
         <div className="layout__row"></div>
         <div className="layout__row"></div>
         <div className="layout__row"></div>
+        <div className="layout__row"></div>
       </div>
-      <div className="layout__column"></div>
       <div className="layout__column">
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
+        <div className="layout__row">1</div>
+        <div className="layout__row">2</div>
+        <div className="layout__row">3</div>
       </div>
-      <div className="layout__column"></div>
       <div className="layout__column">
         <div className="layout__row">MENU</div>
         <div className="layout__row"></div>
         <div className="layout__row"></div>
         <div className="layout__row"></div>
         <div className="layout__row"></div>
+        <div className="layout__row"></div>
       </div>
     </div>
+    // <div className="layout">
+    //   <div className="layout__column">
+    //     <div className="layout__row">SUMI SEO</div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //   </div>
+    //   <div className="layout__column"></div>
+    //   <div className="layout__column">
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //   </div>
+    //   <div className="layout__column"></div>
+    //   <div className="layout__column">
+    //     <div className="layout__row">MENU</div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //     <div className="layout__row"></div>
+    //   </div>
+    // </div>
   );
 };
 
