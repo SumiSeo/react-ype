29b11541b191ed2629100dde79531d33de0cd771
Sumi <qkobr94@gmail.com>
2022-11-23 22:03:00 +0900

    clean layout code with grid solution

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,53 +4,29 @@ const Layout = (): JSX.Element => {
   return (
     <div className="layout">
       <div className="layout__column">
-        <div className="layout__row--absolute"></div>
         <div className="layout__row">SUMI SEO</div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
+        <div className="layout__row layout__row--content">1</div>
+        <div className="layout__row layout__row--content">2</div>
+        <div className="layout__row layout__row--content">3</div>
+        <div className="layout__row layout__row--content">4</div>
+        <div className="layout__row layout__row--content">5</div>
+        <div className="layout__row "></div>
       </div>
       <div className="layout__column">
-        <div className="layout__row">1</div>
-        <div className="layout__row">2</div>
-        <div className="layout__row">3</div>
+        <div className="layout__row"></div>
+        <div className="layout__row layout__row--content"></div>
+        <div className="layout__row">Hello@ </div>
       </div>
       <div className="layout__column">
         <div className="layout__row">MENU</div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
-        <div className="layout__row"></div>
+        <div className="layout__row layout__row--content">1</div>
+        <div className="layout__row layout__row--content">2</div>
+        <div className="layout__row layout__row--content">3</div>
+        <div className="layout__row layout__row--content">4</div>
+        <div className="layout__row layout__row--content">5</div>
+        <div className="layout__row "></div>
       </div>
     </div>
-    // <div className="layout">
-    //   <div className="layout__column">
-    //     <div className="layout__row">SUMI SEO</div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //   </div>
-    //   <div className="layout__column"></div>
-    //   <div className="layout__column">
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //   </div>
-    //   <div className="layout__column"></div>
-    //   <div className="layout__column">
-    //     <div className="layout__row">MENU</div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //     <div className="layout__row"></div>
-    //   </div>
-    // </div>
   );
 };
 
