f3724131c17b8acca25c27fd59a4db039dc4f347
Sumi <qkobr94@gmail.com>
2022-11-29 23:06:44 +0900

    create error page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,22 @@
 import "../scss/main.scss";
-import Layout from "../Layout/Layout";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import ErrorPage from "../ErrorPage/ErroprPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
 const App = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
