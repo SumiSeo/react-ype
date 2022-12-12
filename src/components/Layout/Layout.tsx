ccf4df90635634f38628096bfffd481a0e9e1779
Sumi <qkobr94@gmail.com>
2022-12-13 03:07:22 +0900

    feat(block): modify block with mouse over and out event

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MainBlockComp from "../MainBlockComp/MainBlockComp";
 import "./Layout.scss";
 
 const Layout = (): JSX.Element => {
+  const [right, setRight] = useState<string[]>(["#5", "#4", "#3", "#2", "#1"]);
   const left = ["#10", "#9", "#8", "#7", "#6"];
-  const right = ["#5", "#4", "#3", "#2", "#1"];
+
   const profile =
     " • sumi seo • software engineer • frontend developer • react •typescript • graphql • scss • javascript(es6+) • flutter • aws • sumi seo • dedicated software engineer • frontend developer • react • typescript • graphql • scss • javascript(es6+) • flutter • aws • sumi seo • dedicated software engineer • frontend developer • react• typescript • graphql • scss • javascript(es6+) • flutter • aws";
-  const onMouseOver = (e: React.MouseEvent<HTMLLIElement>) => {
+
+  const onMouseOver = (name: string, i: number) => {
+    //select correct event of list and change it when it is hover
+    console.log("name", name);
+    console.log("i", i);
+    if (i === 0) {
+      setRight(["react", "#4", "#3", "#2", "#1"]);
+    }
+  };
+
+  const onMouseOut = (e: React.MouseEvent<HTMLLIElement>) => {
     console.log(e);
+    console.log("mouse out");
+    //select correct event of list and change it when it is hover
+    setRight(["#5", "#4", "#3", "#2", "#1"]);
   };
 
   return (
@@ -32,14 +46,14 @@ const Layout = (): JSX.Element => {
         <div className="layout__row layout__row--footer">
           <span>{profile}</span>
         </div>
-        4
       </div>
       <div className="layout__column">
         <div className="layout__row layout__row--content">MENU</div>
-        {right.map((name) => {
+        {right.map((name, i) => {
           return (
             <li
-              onMouseOver={onMouseOver}
+              onMouseOut={onMouseOut}
+              onMouseOver={() => onMouseOver(name, i)}
               key={name}
               className="layout__row layout__row--content"
             >
