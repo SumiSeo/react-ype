f6a416f1f944a9fa6fd0b6e00ad9367a555ad9e9
Sumi <qkobr94@gmail.com>
2022-12-14 03:04:13 +0900

    feat(mainBlock): add position css to right in main block

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,17 +1,15 @@
 import "./Title.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Title = (): JSX.Element => {
   return (
     <div>
       <h1 className="title">
-        <a target="_blank" href="https://www.linkedin.com/in/sumiseo/">
-          <FontAwesomeIcon
-            className="title__icon"
-            icon={faArrowRight}
-            fa-thin
-          />
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/sumiseo/"
+        >
+          &#8594;
         </a>
       </h1>
     </div>
