8ee468d8e6758bf704fce140c07596c9a0669028
Sumi <qkobr94@gmail.com>
2022-12-14 02:42:39 +0900

    feat(mainBlock): add fontawesome icon

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,11 +1,17 @@
 import "./Title.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Title = (): JSX.Element => {
   return (
     <div>
       <h1 className="title">
         <a target="_blank" href="https://www.linkedin.com/in/sumiseo/">
-          hi
+          <FontAwesomeIcon
+            className="title__icon"
+            icon={faArrowRight}
+            fa-thin
+          />
         </a>
       </h1>
     </div>
