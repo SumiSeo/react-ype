0dc4fbbc37fea9aca7d41e2f6ede2ae94e19d09e
Sumi <qkobr94@gmail.com>
2022-12-20 16:50:10 +0900

    feat(component): create other components related to react-router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,11 +9,13 @@ import Layout from "../Layout/Layout";
 import ErrorPage from "../ErrorPage/ErroprPage";
 import LinkedInComp from "../LinkedInComp/LinkedInComp";
 import GitHubComp from "../GitHubComp/GitHubComp";
+import DefaultComp from "../DefaultComp/DefaultComp";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index element={<LinkedInComp />} />
+      <Route index element={<DefaultComp />} />
+      <Route path="/LinkedIn" element={<LinkedInComp />} />
       <Route path="/GitHub" element={<GitHubComp />} />
     </Route>
   )
---
+++ src/components/DefaultComp/DefaultComp.tsx
@@ -0,0 +1,12 @@
+import MainBlockComp from "../MainBlockComp/MainBlockComp";
+// import video from "../video/passing.mp4";
+
+const DefaultComp = () => {
+  return (
+    <>
+      <MainBlockComp />
+    </>
+  );
+};
+
+export default DefaultComp;
