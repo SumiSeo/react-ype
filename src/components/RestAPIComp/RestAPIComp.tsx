4c501b2dede23a655d4476e5e9700b3a59f99010
Sumi <qkobr94@gmail.com>
2022-12-22 21:18:38 +0900

    feat(scss/rest): add scss and rest api component

--- src/components/RestAPIComp/RestAPIComp.tsx
+++ src/components/RestAPIComp/RestAPIComp.tsx
@@ -1,9 +1,29 @@
+import video from "../video/weather2.mp4";
+
 const RestAPIComp = () => {
   return (
     <>
+      <h1 className="block__link">
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://sumiseo.github.io/weather-up/"
+        >
+          Weather Up!
+        </a>
+      </h1>
       <div className="mainBlock__img">
-        <img />
+        <video className="mainBlock__video" autoPlay muted loop>
+          <source src={video} type="video/mp4" />
+        </video>
       </div>
+      <a
+        rel="noreferrer"
+        target="_blank"
+        href="https://sumiseo.github.io/weather-up/"
+      >
+        hi
+      </a>
     </>
   );
 };
--- src/components/SCSSComp/SCSSComp.tsx
+++ src/components/SCSSComp/SCSSComp.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
+import "./SCSSCompnent.scss";
 import scss1 from "../../img/scss1.png";
 import scss2 from "../../img/scss2.png";
 import scss3 from "../../img/scss3.png";
 
 const SCSSComp = () => {
+  const images = [scss1, scss2, scss3];
+  const [imageIndex, setImageIndex] = useState<number>(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setImageIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    }, 600);
+  }, []);
   return (
     <>
+      <h1 className="block__link">
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://sumiseo.github.io/wonhundred-clonecoding/"
+        >
+          Won Hundred
+        </a>
+      </h1>
       <div className="block__img">
-        <img src={scss3} alt="space" />
+        <img src={images[imageIndex]} alt="space" />
       </div>
     </>
   );
