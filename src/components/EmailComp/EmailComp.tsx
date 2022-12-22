9805672f8a807afda6becbcebfada941cff3feb0
Sumi <qkobr94@gmail.com>
2022-12-22 18:52:57 +0900

    chore(layout): add scss styling

--- src/components/EmailComp/EmailComp.tsx
+++ src/components/EmailComp/EmailComp.tsx
@@ -4,7 +4,7 @@ import video from "../video/coffee.mp4";
 const EmailComp = () => {
   return (
     <>
-      <Title arr="&#8595;" page="https://github.com/SumiSeo" name="email" />
+      <Title arr="&#8595;" page="mailto: equepem@gmail.com" name="email" />
       <div className="mainBlock__img">
         <video className="mainBlock__video" autoPlay muted loop>
           <source src={video} type="video/mp4" />
