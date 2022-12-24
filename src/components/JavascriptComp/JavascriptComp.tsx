e761aea8b1d0bb151b864d5b775adb5eb1d56114
Sumi <qkobr94@gmail.com>
2022-12-25 03:47:51 +0900

    chore(img): change harry potter images

--- src/components/JavascriptComp/JavascriptComp.tsx
+++ src/components/JavascriptComp/JavascriptComp.tsx
@@ -1,6 +1,18 @@
-import video from "../video/xmas.mp4";
+import { useEffect, useState } from "react";
+// import "./JavascriptCompnent.scss";
+import harrypotter1 from "../../img/harrypotter1.jpg";
+import harrypotter2 from "../../img/harrypotter2.jpg";
+import harrypotter3 from "../../img/harrypotter3.jpg";
 
 const JavascriptComp = () => {
+  const images = [harrypotter1, harrypotter2, harrypotter3];
+  const [imageIndex, setImageIndex] = useState<number>(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setImageIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    }, 600);
+  }, [images.length]);
   return (
     <>
       <h1 className="block__link">
@@ -9,13 +21,11 @@ const JavascriptComp = () => {
           target="_blank"
           href="https://sumiseo.github.io/mfm/"
         >
-          Harry Potter
+          Harry potter
         </a>
       </h1>
-      <div className="mainBlock__img">
-        <video className="mainBlock__video" autoPlay muted loop>
-          <source src={video} type="video/mp4" />
-        </video>
+      <div className="block__img">
+        <img src={images[imageIndex]} alt="space" />
       </div>
     </>
   );
