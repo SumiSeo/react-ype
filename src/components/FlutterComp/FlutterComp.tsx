ef985d9702bc62d33fb4b23d1a1966fecd17a7b6
Sumi <qkobr94@gmail.com>
2022-12-29 01:33:55 +0900

    feat(flutter): add flutter comp and img

--- src/components/FlutterComp/FlutterComp.tsx
+++ src/components/FlutterComp/FlutterComp.tsx
@@ -1,9 +1,16 @@
-const FluttterComp = () => {
+import video from "../video/cook.mp4";
+
+const FlutterComp = () => {
   return (
     <>
-      <div className="mainBlock__img"></div>
+      <h1 className="block__link">Work in Progres...</h1>
+      <div className="mainBlock__img">
+        <video className="mainBlock__video" autoPlay muted loop>
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
     </>
   );
 };
 
-export default FluttterComp;
+export default FlutterComp;
