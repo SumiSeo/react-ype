5e84140fd5f1fa01de5a396aa227a10e62ee2ff7
Sumi <qkobr94@gmail.com>
2022-12-29 00:12:46 +0900

    feat(cadreDuMonde): add react component and images

--- src/components/ReactComp/ReactComp.tsx
+++ src/components/ReactComp/ReactComp.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+// import "./JavascriptCompnent.scss";
+import world1 from "../../img/world1.jpg";
+import world2 from "../../img/world2.jpg";
+import world3 from "../../img/world3.jpg";
+
 const ReactComp = () => {
+  const images = [world1, world2, world3];
+  const [imageIndex, setImageIndex] = useState<number>(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setImageIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    }, 600);
+  }, [images.length]);
   return (
     <>
-      <div className="mainBlock__img"></div>
+      <h1 className="block__link">
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://cosmic-sopapillas-83b536.netlify.app/"
+        >
+          Cadre du Monde
+        </a>
+      </h1>
+      <div className="block__img">
+        <img src={images[imageIndex]} alt="space" />
+      </div>
     </>
   );
 };
