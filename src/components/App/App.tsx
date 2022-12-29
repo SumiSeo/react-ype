f6c8d483267652178ab6564a38d68135341187f9
Sumi <qkobr94@gmail.com>
2022-12-29 21:29:13 +0900

    fix(lazyLoading): delete late loading logic cause of errors appearing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import "../scss/main.scss";
 import {
   createBrowserRouter,
@@ -7,31 +7,18 @@ import {
   Route,
 } from "react-router-dom";
 import Layout from "../Layout/Layout";
-// import LoadingComp from "../LoadingComp/LoadingComp";
-// import LinkedInComp from "../LinkedInComp/LinkedInComp";
-// import GitHubComp from "../GitHubComp/GitHubComp";
-// import DefaultComp from "../DefaultComp/DefaultComp";
-// import MobileComp from "../MobileComp/MobileComp";
-// import EmailComp from "../EmailComp/EmailComp";
-// import FlutterComp from "../FlutterComp/FlutterComp";
-// import TypescriptComp from "../TypescriptComp/TypescriptComp";
-// import SCSSComp from "../SCSSComp/SCSSComp";
-// import JavascriptComp from "../JavascriptComp/JavascriptComp";
-// import ReactComp from "../ReactComp/ReactComp";
-// import RestAPIComp from "../RestAPIComp/RestAPIComp";
-
-const LinkedInComp = lazy(() => import("../LinkedInComp/LinkedInComp"));
-const GitHubComp = lazy(() => import("../GitHubComp/GitHubComp"));
-const DefaultComp = lazy(() => import("../DefaultComp/DefaultComp"));
-const LoadingComp = lazy(() => import("../LoadingComp/LoadingComp"));
-const MobileComp = lazy(() => import("../MobileComp/MobileComp"));
-const EmailComp = lazy(() => import("../EmailComp/EmailComp"));
-const FlutterComp = lazy(() => import("../FlutterComp/FlutterComp"));
-const TypescriptComp = lazy(() => import("../TypescriptComp/TypescriptComp"));
-const SCSSComp = lazy(() => import("../SCSSComp/SCSSComp"));
-const JavascriptComp = lazy(() => import("../JavascriptComp/JavascriptComp"));
-const ReactComp = lazy(() => import("../ReactComp/ReactComp"));
-const RestAPIComp = lazy(() => import("../RestAPIComp/RestAPIComp"));
+import LoadingComp from "../LoadingComp/LoadingComp";
+import LinkedInComp from "../LinkedInComp/LinkedInComp";
+import GitHubComp from "../GitHubComp/GitHubComp";
+import DefaultComp from "../DefaultComp/DefaultComp";
+import MobileComp from "../MobileComp/MobileComp";
+import EmailComp from "../EmailComp/EmailComp";
+import FlutterComp from "../FlutterComp/FlutterComp";
+import TypescriptComp from "../TypescriptComp/TypescriptComp";
+import SCSSComp from "../SCSSComp/SCSSComp";
+import JavascriptComp from "../JavascriptComp/JavascriptComp";
+import ReactComp from "../ReactComp/ReactComp";
+import RestAPIComp from "../RestAPIComp/RestAPIComp";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -52,11 +39,7 @@ const router = createBrowserRouter(
 );
 
 const App = (): JSX.Element => {
-  return (
-    <Suspense fallback={<LoadingComp />}>
-      <RouterProvider router={router} />{" "}
-    </Suspense>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
